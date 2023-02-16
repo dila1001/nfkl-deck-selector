7842cbf4a678217e5466c2cedd2378c6b6387684
Adila Razmi <adilarazmi@Adilas-MBP.localdomain>
2023-02-16 13:59:02 +0100

    Initialized project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./public/public.module').then((m) => m.PublicModule),
+    // canActivate: [LoginGuard],
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./private/private.module').then((m) => m.PrivateModule),
+    // canActivate: [AuthGuard],
+  },
+  // redirect to public if no path is provided
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
