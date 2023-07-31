4c54f5787d0b923cf8f08b092c62d5b6b7f88d74
Halif Bakri <halif.bakri@gmail.com>
2023-07-31 17:54:04 +0200

    Remove tutorial code

--- api/datalayer/user_db.py
+++ api/datalayer/user_db.py
@@ -22,11 +22,3 @@ def create_user(db: Session, user: User):
     db.commit()
     db.refresh(user)
     return user
-
-# def create_user(db: Session, user: schemas.UserCreate):
-#     fake_hashed_password = user.password + "notreallyhashed"
-#     db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
-#     db.add(db_user)
-#     db.commit()
-#     db.refresh(db_user)
-#     return db_user
